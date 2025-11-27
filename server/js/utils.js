0d570f73302f4c8c4cc45fcc3c24d6261564270f
bondyuk777 <natalavorob506@gmail.com>
2025-11-27 04:40:40 +0200

    Update utils.js

--- server/js/utils.js
+++ server/js/utils.js
@@ -1,13 +1,11 @@
 
 var Utils = {},
-    sanitizer = require('sanitizer'),
     Types = require("../../shared/js/gametypes");
 
 module.exports = Utils;
 
 Utils.sanitize = function(string) {
-    // Strip unsafe tags, then escape as html entities.
-    return sanitizer.escape(sanitizer.sanitize(string));
+    return String(string);
 };
 
 Utils.random = function(range) {
